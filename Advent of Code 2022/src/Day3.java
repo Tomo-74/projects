39123f9bd02a326825c2715924498da522817007
Thomas Lonowski <tomo.lonowski@gmail.com>
2022-12-11 17:56:46 -0700

    Finished day3

--- Advent of Code 2022/src/Day3.java
+++ Advent of Code 2022/src/Day3.java
@@ -23,7 +23,6 @@ public class Day3 {
 		File input=new File("C:\\Users\\tomol\\git\\Personal\\Advent of Code 2022\\Input\\Day3.txt");
 		Scanner lineCheck=new Scanner(input);
 		Scanner scan=new Scanner(input);
-		ArrayList<String> matches=new ArrayList<>();
 		int sum=0;
 		
 		while(hasNext3Lines(lineCheck)) {
@@ -38,30 +37,28 @@ public class Day3 {
 			ArrayList<String> aL1=new ArrayList<>();
 			ArrayList<String> aL2=new ArrayList<>();
 			ArrayList<String> aL3=new ArrayList<>();
-			char[] targetChars=new char[52];
-			
+			String[] targetChars= {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+								 "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};			
 			String commonChar;
 			
+			// Populate the lists with input lines
 			for(int i=0; i<len1; i++) { aL1.add(firstLine.substring(i,i+1)); }
 			for(int j=0; j<len2; j++) { aL2.add(secondLine.substring(j,j+1)); }
 			for(int k=0; k<len3; k++) { aL3.add(thirdLine.substring(k,k+1)); }
 
-			for(int l=0; l<targetChars.length; l++) {
-				if(aL1.contains(targetChars[l]) && aL2.contains(targetChars[l]) && aL3.contains(targetChars[l])) {
-					commonChar=Character.targetChars[l];
+			// Find the common char and calculate the sum of the priorities
+			for(int i=0; i<targetChars.length; i++) {
+				if(aL1.contains(targetChars[i]) && aL2.contains(targetChars[i]) && aL3.contains(targetChars[i])) {
+					commonChar=targetChars[i];
+					//Calculate priority value based on char's ascii value and whether it's upper or lower case
+					if(Character.isUpperCase(commonChar.charAt(0))) sum+=(int)(commonChar.charAt(0))-38;
+					else sum+=((int)(commonChar.charAt(0))-96);
 					break;
 				}
 			}
-			System.out.println(commonChar);		}
-		/*
-		for(String s : matches) {
-			char curChar=s.charAt(0);
-			if(Character.isUpperCase(curChar)) sum+=(int)curChar-38;
-			else sum+=((int)curChar-96);
 		}
-		scan.close();
 		System.out.println(sum);
-		*/
+		scan.close();
 	}
 }
 
